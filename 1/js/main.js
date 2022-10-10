5445387c4856a0c87bf2635fad91ea44ab3b13f6
calvinyardinm2i <calvin59600@gmail.com>
2022-10-10 09:26:14 +0200

    dfggdfdfg2dfg5df

---
+++ 1/js/main.js
@@ -0,0 +1,8 @@
+console.log(document.getElementById("my-id"))
+console.log(document.getElementsByClassName("my-class")[1])
+
+
+for (el of document.getElementsByTagName("li"))
+{
+	console.log(el)
+}
